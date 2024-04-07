46ff9ed7ed0bb72caede5380b00cd314d237b6dd
Nandhini_T <129613480+NandhuTee@users.noreply.github.com>
2024-04-08 00:16:58 +0530

    Update TooltipPortal.js

--- components/TooltipPortal.js
+++ components/TooltipPortal.js
@@ -1,9 +1,11 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
 
 const portalRoot = typeof document !== 'undefined' ? document.getElementById('tooltip-portal') : null;
 
 function TooltipPortal({ children }) {
-
+  const portal = portalRoot ? ReactDOM.createPortal(children, portalRoot) : null;
+  return portal;
 }
 
 export default TooltipPortal;
